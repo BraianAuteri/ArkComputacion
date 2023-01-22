03c0ff318186ed9b143105a72b935ee00e6ca622
Auteri Braian <braianauteri@gmail.com>
2023-01-22 14:13:57 -0300

    arreglo en footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
                     <div>
                         <h3>Nuestras Redes:</h3>
                             <div className='redes justify-content-center'>
-                                <a href="https://www.facebook.com" target="_blank"><FacebookIcon /></a>
-                                <a href="https://www.instagram.com" target="_blank"> <InstagramIcon /></a>
+                                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FacebookIcon /></a>
+                                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"> <InstagramIcon /></a>
                             </div>
                     </div>
                 </div>
